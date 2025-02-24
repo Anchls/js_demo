6657ccc1140a2078e3408bff2749e8377b953233
aanchal sharma <anchlshrma2003@gmail.com>
2025-02-24 22:22:32 +0530

    concersion

---
+++ conversion.js
@@ -0,0 +1,9 @@
+let score=33
+let stringNumber=String(score)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
+let Name="123"
+let AS=Boolean(Name)
+console.log(typeof Name)
+console.log(AS)
