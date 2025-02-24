0787a7d7f63d2752ed58f14013c30202d9959ef9
aanchal sharma <anchlshrma2003@gmail.com>
2025-02-24 23:33:50 +0530

    commit2

--- conversion.js
+++ conversion.js
@@ -1,9 +1,9 @@
 let score=33
 let stringNumber=String(score)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+console.log(stringNumber)   //33
+console.log(typeof stringNumber)   //string
 
 let Name="123"
 let AS=Boolean(Name)
-console.log(typeof Name)
-console.log(AS)
+console.log(typeof Name) //string
+console.log(AS)   //true
