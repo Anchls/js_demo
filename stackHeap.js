0bc10fb630eadb23ad07a76e001e659a465dd797
aanchal sharma <anchlshrma2003@gmail.com>
2025-02-25 00:55:49 +0530

    stack and heap

---
+++ stackHeap.js
@@ -0,0 +1,11 @@
+//stack-primitive and heap-non primitive
+
+let my = "anchl"
+let another=my;
+another="aman";
+console.log(my)
+console.log(another)
+
+
+//heap k under jo bhi dalte hai humko uska reference milta hai
+//stack mai jo bi jata hai humko uska referene milta hai
